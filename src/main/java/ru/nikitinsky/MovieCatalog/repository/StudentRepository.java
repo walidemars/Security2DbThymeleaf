00207808df4e348457dde8de9b97b8f3528189b5
walidemars <walidemars52@gmail.com>
2026-01-21 19:34:43 +0500

    done

--- src/main/java/ru/nikitinsky/MovieCatalog/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.nikitinsky.MovieCatalog.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.nikitinsky.MovieCatalog.entity.Student;
-
-@Repository
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
